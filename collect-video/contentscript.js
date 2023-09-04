653afd096a56270791dee6586f2452be9c3067ea
chenwei <294098494@qq.com>
2023-09-04 18:40:56 +0800

    fix: 视频采集

--- collect-video/contentscript.js
+++ collect-video/contentscript.js
@@ -1,29 +1,5 @@
 var s = document.createElement('script');
 // must be listed in web_accessible_resources in manifest.json
-s.src = chrome.runtime.getURL('common.js');
-s.onload = function () {
-    this.remove();
-};
+s.src = chrome.runtime.getURL('com.js');
 (document.head || document.documentElement).appendChild(s);
 
-
-if (location.href.indexOf('.com') !== -1) {
-    let s = document.createElement('script');
-    // must be listed in web_accessible_resources in manifest.json
-    s.src = chrome.runtime.getURL('com.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-}
-
-
-if (location.href.indexOf('.club') !== -1) {
-    let s = document.createElement('script');
-    // must be listed in web_accessible_resources in manifest.json
-    s.src = chrome.runtime.getURL('club.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-}
