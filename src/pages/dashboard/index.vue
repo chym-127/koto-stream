44787d1939e1d2b85f05723d87617273825d17bd
chenwei <294098494@qq.com>
2023-09-23 16:48:36 +0800

    feat: 可筛选

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="full vod-list container">
-    <div class="vod-item" v-for="(item, index) in items" :key="index" @click="jumpToDetails(item)">
-      <div class="pic">
-        <img :src="item.poster_url" alt="" srcset="" loading="lazy"/>
-      </div>
-      <div class="info mt-4 c-000 ellips-2" style="word-break: break-all">
-        <span class="font-14-600">{{ item.title }}</span>
+  <div style="position: relative; overflow: hidden">
+    <div class="flex flex-row" style="position: absolute; width: 100vw; height: 90px; padding: 24px 100px">
+      <a-select v-model:value="listReqForm.type" @change="onSearch" size="large">
+        <a-select-option :value="0">全部</a-select-option>
+        <a-select-option :value="1">电影</a-select-option>
+        <a-select-option :value="2">电视剧</a-select-option>
+      </a-select>
+      <a-input-search
+        placeholder="按照剧名搜索"
+        style="margin-left: 12px"
+        v-model:value="listReqForm.keywords"
+        size="large"
+        @search="onSearch"
+      >
+        <a-button slot="enterButton">搜索</a-button>
+      </a-input-search>
+    </div>
+    <div class="full vod-list container" style="margin-top: 90px; overflow: auto; height: calc(640px - 122px)">
+      <div class="vod-item" v-for="(item, index) in items" :key="index">
+        <div class="pic" @click="jumpToDetails(item)">
+          <img :src="item.poster_url" alt="" srcset="" loading="lazy" />
+        </div>
+        <div class="info mt-4 c-000 ellips-2" style="word-break: break-all">
+          <span class="font-14-600">{{ item.title }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -18,11 +36,26 @@ import { listMedia } from '../../api/index';
 import { getMediaLocalResouce } from '../../utils';
 const router = useRouter();
 let items = reactive<VideoInfo[]>([]);
-let currentPoster = ref('');
+
+interface ListReq {
+  keywords: string;
+  current: number;
+  page_limit: number;
+  type: number;
+}
+
+const listReqForm = reactive<ListReq>({
+  keywords: '',
+  current: 1,
+  page_limit: 20,
+  type: 0
+});
 
 function handleListVideo() {
-  items.splice(0);
-  listMedia({}).then((resp) => {
+  listMedia(listReqForm).then((resp) => {
+    if (listReqForm.current === 1) {
+      items.splice(0);
+    }
     Object.assign(items, resp.data);
     for (let index = 0; index < items.length; index++) {
       const video = items[index];
@@ -42,14 +75,11 @@ function jumpToDetails(row: any) {
   });
 }
 
-function mouseenter(event: any, item: VideoInfo) {
-  let localUrl = getMediaLocalResouce(item, 'fanart.jpg');
-  if (localUrl) {
-    currentPoster.value = localUrl;
-  }
+function onSearch() {
+  listReqForm.current = 1;
+  handleListVideo();
 }
 
-function mouseleave(event: any, item: VideoInfo) {}
 handleListVideo();
 </script>
 
@@ -83,11 +113,11 @@ handleListVideo();
 
 .container > .vod-item {
   width: 210px;
-  height: 300px;
+  height: 260px;
   padding: 20px 20px 0 20px;
-  cursor: pointer;
 
   .pic {
+    cursor: pointer;
     border-radius: 12px;
     width: 170px;
     height: 220px;
